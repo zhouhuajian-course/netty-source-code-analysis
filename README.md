9d09e4210f063825cfce668f82308aec622413b6
zhouhuajian <zhouhuajian@company.com>
2023-04-04 17:04:21 +0800

    增加Netty测试

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Netty 源码分析
 
+https://github.com/netty/netty
+
---
+++ netty-test/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Hello world!
+ *
+ */
+@Slf4j
+public class App 
+{
+    public static void main( String[] args )
+    {
+        log.debug("123");
+        System.out.println( "Hello World!" );
+    }
+}
